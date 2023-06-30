8638738c87e5e70e84efa79da8075d89c47f3ec5
Paree2512 <benjaminsemevor@ymail.com>
2023-06-30 22:05:02 +0000

    Utilizing the string format specifier %s

---
+++ Hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ * Return: 0
+ */
+
+char main(void)
+{
+	char i[] = "Hello World";
+
+	printf("This program will print %s\n", i);
+
+	return (0);
+}
